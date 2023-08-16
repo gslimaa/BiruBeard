c5d5ab1c87b0513e1ddd3d361274bea61b834d8c
gslimaa <142360639+gslimaa@users.noreply.github.com>
2023-08-15 21:14:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,22 +18,33 @@ st.set_page_config(page_title='BiruBeard',page_icon=favicon,layout="wide", initi
 @st.cache_data
 def importar_agendamentos():
     df=pd.read_excel(r"base_agendamentos.xlsx",header=7)
+    df.drop('Unnamed: 0', axis=1,inplace=True)
+    df.drop('Unnamed: 1', axis=1,inplace=True)
+    df['Data e hora']=pd.to_datetime(df['Data e hora'],dayfirst=True)
+    df['ano'] = pd.DatetimeIndex(df['Data e hora']).year
+    df['mes'] = pd.DatetimeIndex(df['Data e hora']).month
     #df_limpo= df.astype(str) # convertendo todas as colunas em str
     return df
 
 @st.cache_data
 def importar_clientes():
     df=pd.read_excel(r"base_clientes.xlsx",header=7)
+    df.drop('Unnamed: 0', axis=1,inplace=True)
+    df.drop('Unnamed: 1', axis=1,inplace=True)
+    df['Primeira visita']=pd.to_datetime(df['Primeira visita'],dayfirst=True)
+    df['Ultima visita']=pd.to_datetime(df['Ultima visita'],dayfirst=True)
     #df_limpo= df.astype(str) # convertendo todas as colunas em str
     return df
 
 df_agendamentos = importar_agendamentos()
-
 df_clientes = importar_clientes()
+st.title ('BiruBeard Analytics')
 
-st.write(df_agendamentos.shape)
-df_agendamentos
+fat_por_ano = px.bar(df_agendamentos[['ano','Receita total']].groupby(by=['ano']).sum().reset_index(), x="ano", y="Receita total",text_auto=True, title='Faturamento por ano')
+st.plotly_chart(fat_por_ano, use_container_width=True,)
+#st.write(df_agendamentos.shape)
+#df_agendamentos
 st.write("----")
 st.write("###")
-st.write(df_clientes.shape)
-df_clientes
+#st.write(df_clientes.shape)
+#df_clientes
