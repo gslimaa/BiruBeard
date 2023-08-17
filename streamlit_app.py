11cd25cb5ae5c67374b17ad78fef374fe4915244
gslimaa <142360639+gslimaa@users.noreply.github.com>
2023-08-16 21:34:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,13 @@ import io
 from pyxlsb import open_workbook as open_xlsb
 import numpy as np
 from st_pages import show_pages_from_config
+from plotly.subplots import make_subplots
 
 favicon = Image.open('logo.jfif')
 st.set_page_config(page_title='BiruBeard',page_icon=favicon,layout="wide", initial_sidebar_state="collapsed")
 
 #@st.cache_data.clear()
-@st.cache_data
+@st.cache_data(ttl=3600)
 def importar_agendamentos():
     df=pd.read_excel(r"base_agendamentos.xlsx",header=7)
     df.drop('Unnamed: 0', axis=1,inplace=True)
@@ -23,16 +24,20 @@ def importar_agendamentos():
     df['Data e hora']=pd.to_datetime(df['Data e hora'],dayfirst=True)
     df['ano'] = pd.DatetimeIndex(df['Data e hora']).year
     df['mes'] = pd.DatetimeIndex(df['Data e hora']).month
+    last_line=df.tail(1).index.values.astype(int)
+    df.drop(last_line,inplace=True)
     #df_limpo= df.astype(str) # convertendo todas as colunas em str
     return df
 
-@st.cache_data
+@st.cache_data(ttl=3600)
 def importar_clientes():
     df=pd.read_excel(r"base_clientes.xlsx",header=7)
     df.drop('Unnamed: 0', axis=1,inplace=True)
     df.drop('Unnamed: 1', axis=1,inplace=True)
     df['Primeira visita']=pd.to_datetime(df['Primeira visita'],dayfirst=True)
     df['Ultima visita']=pd.to_datetime(df['Ultima visita'],dayfirst=True)
+    last_line=df.tail(1).index.values.astype(int)
+    df.drop(last_line,inplace=True)
     #df_limpo= df.astype(str) # convertendo todas as colunas em str
     return df
 
@@ -53,7 +58,32 @@ with col2:
     faturamento_ate_mes_atual_ano_atual=faturamento_ate_mes_atual[faturamento_ate_mes_atual['ano']==ano_atual]['Receita total'].values[0]
     faturamento_ate_mes_atual_ano_anterior=faturamento_ate_mes_atual[faturamento_ate_mes_atual['ano']==ano_atual-1]['Receita total'].values[0]
     crescimento_ytd = (faturamento_ate_mes_atual_ano_atual-faturamento_ate_mes_atual_ano_anterior)/faturamento_ate_mes_atual_ano_anterior
-    st.write(f'No Year to Date, até o mês atual, temos um crescimento de: **{round(crescimento_ytd*100,1)}%**, com **R${round(faturamento_ate_mes_atual_ano_atual-faturamento_ate_mes_atual_ano_anterior,2)}** faturado a mais que no mesmo período do ano anterior.')
+    st.metric(label="Faturamento atual:", value=f"R${faturamento_ate_mes_atual_ano_atual:,.2f}", delta=f"{crescimento_ytd:.2%}")
+col1,col2=st.columns([1,9])
+with col1:
+    col11,col22=st.columns([1,1])
+    with col11:
+        selecao_ano=st.radio("Selecione o ano:" ,df_agendamentos['ano'].unique().astype(int))
+    with col22:
+        selecao_mes=st.radio("Selecione o mês:" ,df_agendamentos[df_agendamentos['ano']==selecao_ano]['mes'].unique().astype(int))
+with col2:
+    df_vendas_por_categoria_barbeiro=df_agendamentos[(df_agendamentos['mes']==selecao_mes) & (df_agendamentos['ano']==selecao_ano)][['Funcionário','Categoria principal','Receita total']].groupby(['Funcionário','Categoria principal']).sum().reset_index()
+    vendas_categoria_barbeiro=make_subplots(rows=1, cols=df_vendas_por_categoria_barbeiro['Funcionário'].nunique(), subplot_titles=df_vendas_por_categoria_barbeiro['Funcionário'].unique())
+    i=1
+    for funcionario in df_vendas_por_categoria_barbeiro['Funcionário'].unique():
+        vendas_categoria_barbeiro.add_trace(go.Bar(
+                name=funcionario,
+                y=df_vendas_por_categoria_barbeiro[df_vendas_por_categoria_barbeiro['Funcionário']==funcionario]['Receita total'],
+                x=df_vendas_por_categoria_barbeiro[df_vendas_por_categoria_barbeiro['Funcionário']==funcionario]['Categoria principal'],
+                text=df_vendas_por_categoria_barbeiro[df_vendas_por_categoria_barbeiro['Funcionário']==funcionario]['Receita total']),
+                #marker=dict(color=cores_personalizadas[0])),
+                row=1, col=i)
+        if i>1:
+            vendas_categoria_barbeiro.update_yaxes(showticklabels=False, row=1, col=i,range=[0,df_vendas_por_categoria_barbeiro['Receita total'].max()*1.1])
+        else:
+            vendas_categoria_barbeiro.update_yaxes(row=1, col=i,range=[0,df_vendas_por_categoria_barbeiro['Receita total'].max()*1.1])
+        i+=1
+    st.plotly_chart(vendas_categoria_barbeiro, use_container_width=True,)
 #st.write(df_agendamentos.shape)
 #df_agendamentos
 st.write("----")
