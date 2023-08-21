de68ca5e57b2cab11cd9e5d436a996143e4aa2b0
gslimaa <142360639+gslimaa@users.noreply.github.com>
2023-08-21 00:11:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,12 @@ from pyxlsb import open_workbook as open_xlsb
 import numpy as np
 from st_pages import show_pages_from_config
 from plotly.subplots import make_subplots
+import datetime as dt
+from calendar import monthrange
 
 favicon = Image.open('logo.jfif')
 st.set_page_config(page_title='BiruBeard',page_icon=favicon,layout="wide", initial_sidebar_state="collapsed")
-#ok
+
 #@st.cache_data.clear()
 @st.cache_data(ttl=3600)
 def importar_agendamentos():
@@ -45,10 +47,12 @@ df_agendamentos = importar_agendamentos()
 df_clientes = importar_clientes()
 st.title ('BiruBeard Analytics')
 
+dict_meses={1:'jan',2:'fev',3:'mar',4:'abr',5:'mai',6:'jun',7:'jul',8:'ago',9:'set',10:'out',11:'nov',12:'dez'}
+
 col1,col2=st.columns([3,1])
 with col1:
     ano_atual=df_agendamentos['ano'].max()
-    ultimo_mes_ano_atual=df_agendamentos[df_agendamentos['ano']==2023]['mes'].max()
+    ultimo_mes_ano_atual=df_agendamentos[df_agendamentos['ano']==ano_atual]['mes'].max()
     faturamento_ate_mes_atual=df_agendamentos[(df_agendamentos['mes']<=ultimo_mes_ano_atual)][['ano','Receita total']].groupby(by=['ano']).sum().reset_index()
     fat_por_ano = px.bar(df_agendamentos[['ano','Receita total']].groupby(by=['ano']).sum().reset_index(), x="ano", y="Receita total",text_auto=True, title='Faturamento por ano')
     fat_por_ano.add_trace(go.Scatter(x=faturamento_ate_mes_atual['ano'], y=faturamento_ate_mes_atual['Receita total'], mode='lines+markers', text='x', showlegend=False))
@@ -59,6 +63,44 @@ with col2:
     faturamento_ate_mes_atual_ano_anterior=faturamento_ate_mes_atual[faturamento_ate_mes_atual['ano']==ano_atual-1]['Receita total'].values[0]
     crescimento_ytd = (faturamento_ate_mes_atual_ano_atual-faturamento_ate_mes_atual_ano_anterior)/faturamento_ate_mes_atual_ano_anterior
     st.metric(label=f"Faturamento {int(ano_atual)}:", value=f"R${faturamento_ate_mes_atual_ano_atual:,.2f}", delta=f"{crescimento_ytd:.2%} vs {int(ano_atual)-1}")
+    st.write('---')
+    fat_projetado_ano=(faturamento_ate_mes_atual_ano_atual/ultimo_mes_ano_atual)*12
+    tickets_ano_atual=df_agendamentos[(df_agendamentos['ano']==ano_atual) & (df_agendamentos['Status']=='Concluída')]['ID da Reserva'].count()
+    st.write(f'Faturamento projetado {int(ano_atual)}: **R${fat_projetado_ano:,.2f}**')
+    st.write(f'QTD de tickets {int(ano_atual)}: **{tickets_ano_atual}**')
+    st.write(f'Ticket médio {int(ano_atual)}: **R${round(faturamento_ate_mes_atual_ano_atual/tickets_ano_atual,2)}**')
+
+    
+    
+
+col1,col2=st.columns([3,1]) 
+with col1:
+    fig_fat_por_mes_ano_atual=px.bar(df_agendamentos[df_agendamentos['ano']==ano_atual][['mes','Receita total']].groupby(by=['mes']).sum().reset_index(), x="mes", y="Receita total",text_auto=True, title='Faturamento por mês')
+    st.plotly_chart(fig_fat_por_mes_ano_atual, use_container_width=True,)
+with col2:
+
+
+    mes_atual_str=dict_meses[ultimo_mes_ano_atual]
+    #st.write(mes_atual_str)
+    fat_mes_atual=df_agendamentos[(df_agendamentos['mes']==ultimo_mes_ano_atual) & (df_agendamentos['ano']==ano_atual)]['Receita total'].sum()
+    fat_mes_passado=df_agendamentos[(df_agendamentos['mes']==ultimo_mes_ano_atual-1) & (df_agendamentos['ano']==ano_atual)]['Receita total'].sum()
+    crescimento_mes=(fat_mes_atual-fat_mes_passado)/fat_mes_passado
+    fat_mes_atual_ano_passado=df_agendamentos[(df_agendamentos['mes']==ultimo_mes_ano_atual) & (df_agendamentos['ano']==ano_atual-1)]['Receita total'].sum()
+    tickets_mes_atual=df_agendamentos[(df_agendamentos['mes']==ultimo_mes_ano_atual) & (df_agendamentos['ano']==ano_atual) & (df_agendamentos['Status']=='Concluída')]['ID da Reserva'].count()
+    dia_atual=dt.date.today().day
+    ultimo_dia_mes_atual=dt.date.today().replace(day=monthrange(dt.date.today().year,dt.date.today().month)[1]).day
+    fat_projetado_mes_atual=(fat_mes_atual/dia_atual)*ultimo_dia_mes_atual
+    #st.write(ultimo_dia_mes_atual)
+    st.metric(label=f"Faturamento mês {mes_atual_str}:", value=f"R${fat_mes_atual:,.2f}", delta=f"{crescimento_mes:.2%} vs {dict_meses[ultimo_mes_ano_atual-1]}")
+    st.write('---')
+    #st.write(f'Faturamento do mês atual em {ano_atual-1:.0f}: **R${fat_mes_atual_ano_passado:,.2f}**')
+    st.write(f'Faturamento projetado: **R${fat_projetado_mes_atual:,.2f}**')
+    #st.write(dia_atual)
+    st.write(f'QTD de tickets {mes_atual_str}: **{tickets_mes_atual}**')
+    st.write(f'Ticket médio {mes_atual_str}: **R${round(fat_mes_atual/tickets_mes_atual,2)}**')
+    st.write('---')
+
+    
 col1,col2=st.columns([1,9])
 with col1:
     col11,col22=st.columns([1,1])
@@ -75,6 +117,11 @@ with col2:
     selecao_visao=st.radio("Ver por:" ,['Receita total','Porcentagem'])
 
     for funcionario in df_vendas_por_categoria_barbeiro['Funcionário'].unique():
+
+
+
+
+
         vendas_categoria_barbeiro.add_trace(go.Bar(
                 name=funcionario,
                 y=df_vendas_por_categoria_barbeiro[df_vendas_por_categoria_barbeiro['Funcionário']==funcionario][selecao_visao],
@@ -82,6 +129,7 @@ with col2:
                 text=df_vendas_por_categoria_barbeiro[df_vendas_por_categoria_barbeiro['Funcionário']==funcionario][selecao_visao]),
                 #marker=dict(color=cores_personalizadas[0])),
                 row=1, col=i)
+        
         if selecao_visao=='Porcentagem':
             vendas_categoria_barbeiro.update_traces(texttemplate='%{y}%', textposition='inside', textangle=0)
         if i>1:
@@ -89,10 +137,9 @@ with col2:
         else:
             vendas_categoria_barbeiro.update_yaxes(row=1, col=i,range=[0,df_vendas_por_categoria_barbeiro[selecao_visao].max()*1.1])
         i+=1
-    st.plotly_chart(vendas_categoria_barbeiro, use_container_width=True,)
-#st.write(df_agendamentos.shape)
-#df_agendamentos
+        #st.write(funcionario)
+    st.plotly_chart(vendas_categoria_barbeiro, use_container_width=True)
+
 st.write("----")
 st.write("###")
-#st.write(df_clientes.shape)
-#df_clientes
+
