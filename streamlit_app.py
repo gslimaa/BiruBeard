85a9d058e2fe15bd260780c30ca8a15af89a58e4
gslimaa <142360639+gslimaa@users.noreply.github.com>
2023-08-25 01:34:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ import math
 favicon = Image.open('logo.jfif')
 st.set_page_config(page_title='BiruBeard',page_icon=favicon,layout="wide", initial_sidebar_state="collapsed")
 
-#@st.cache_data.clear()
+@st.cache_data.clear()
 @st.cache_data()
 def importar_agendamentos():
     df=pd.read_excel(r"lista_fixa.xlsx",header=7)
